7d778c78406f2760849600e22b5c332a8207abfe
joshcowen <josh@guidemktg.com>
2026-04-27 15:35:23 -0400

    Temp: switch from address to resend.dev for delivery test

--- api/sign.js
+++ api/sign.js
@@ -16,7 +16,7 @@ module.exports = async function handler(req, res) {
 
   try {
     await resend.emails.send({
-      from: 'Guide MKTG Proposals <proposals@guidemktg.com>',
+      from: 'Guide MKTG Proposals <onboarding@resend.dev>',
       to: 'josh@guidemktg.com',
       subject: `Signed: ${proposalTitle}`,
       html: `
